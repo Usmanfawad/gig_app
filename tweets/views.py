fc798050f7eecb0b32a866db615170df186ada6c
usmanfawad <ufawad0@gmail.com>
2020-05-19 21:57:01 +0500

    .

--- tweets/views.py
+++ tweets/views.py
@@ -6,8 +6,17 @@ from .models import *
 def home_view(request,*args,**kwargs):
     return render(request,"pages/home.html",context={},status=200)
 
-def tweet_detail_view(request,tweet_id,*args,**kwargs):
+def tweet_list_view(request,*args,**kwargs):
+    qs= Tweet.objects.all()
+    tweets_list= [{"id": x.id, "content": x.content} for x in qs]
+    data= {
+        "response": tweets_list
+    }
+    return JsonResponse(data) 
 
+
+
+def tweet_detail_view(request,tweet_id,*args,**kwargs):
     #Rest API 
     data={
         "id": tweet_id,
