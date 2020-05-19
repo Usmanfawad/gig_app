54796a655f322b79f074a220c415bc75329a6b77
Usman Fawad <ufawad0@gmail.com>
2020-05-19 20:11:49 +0500

    django initializing app

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tweets.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view), 
+    path('home/<int:tweet_id>',home_view),
 ]
